a5994ba323807d8a287fb91e116c17d30ee44a2e
Jay <jaypee.1797@gmail.com>
2016-11-02 19:07:54 -0700

    Added Selectable Buttons to GameOver View

--- My TicTacToe/GameOverView.swift
+++ My TicTacToe/GameOverView.swift
@@ -12,10 +12,8 @@ import SnapKit
 
 class GameOverView: UIView {
     
-    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        
     }
     
     override init(frame: CGRect) {
@@ -23,8 +21,9 @@ class GameOverView: UIView {
         createView()
     }
     
+    //Create the GameOver View:
     func createView() {
-        
+        //Set the Width & Height constants for the constraints:
         let width = self.bounds.width
         let height = self.bounds.height
         
@@ -41,29 +40,41 @@ class GameOverView: UIView {
             make.edges.equalTo(view).inset(UIEdgeInsetsMake(0, 20, height*0.75, 20))
         }
         
-        //Create Win|Lose|Tie Label:
+        //Create Win||Tie Label:
         let winnerLabel = createLabel(name: "The Winner is: ", textSize: 30, borderWidth: 0)
         view.addSubview(winnerLabel)
         winnerLabel.snp.makeConstraints { (make) -> Void in
             make.edges.equalTo(view).inset(UIEdgeInsetsMake(height*0.25, 20, height*0.5, 20))
         }
         
-        //Create Rematch, Menu & Settings Buttons:
-        let restartView = createLabel(name: "Rematch", textSize: 40, borderWidth: 3)
-        view.addSubview(restartView)
-        restartView.snp.makeConstraints { (make) -> Void in
-            make.edges.equalTo(view).inset(UIEdgeInsetsMake(height*0.5, width*0.25, 300, width*0.25))
+        //Create Menu Label:
+        let menuLabel = createButton(name: "Menu", textSize: 40, borderWidth: 3)
+        view.addSubview(menuLabel)
+        menuLabel.snp.makeConstraints { (make) -> Void in
+            make.bottom.equalTo(self.snp.bottom).offset(-20)
+            make.width.equalTo(215)
+            make.height.equalTo(60)
+            make.centerX.equalTo(self.snp.centerX)
         }
         
-        
+        //Create Rematch Label:
+        let restartLabel = createButton(name: "Rematch", textSize: 30, borderWidth: 3)
+        view.addSubview(restartLabel)
+        restartLabel.addTarget(self, action: #selector(GameOverView.didTapRestart), for: .touchUpInside)
+        restartLabel.snp.makeConstraints { (make) -> Void in
+            make.bottom.equalTo(menuLabel.snp.top).offset(-20)
+            make.width.equalTo(menuLabel.snp.width)
+            make.height.equalTo(60)
+            make.centerX.equalTo(self.snp.centerX)
+        }
         
         //UITapGestureRecognizer Setup
         let tap = UITapGestureRecognizer(target: self, action: #selector(didTapRestart))
-        restartView.addGestureRecognizer(tap)
+        restartLabel.addGestureRecognizer(tap)
     }
     
     
-    //Create Label Function:
+    //Create Labels:
     func createLabel(name: String, textSize: CGFloat, borderWidth: CGFloat) -> UILabel {
         let label = UILabel()
         label.isUserInteractionEnabled = true
@@ -74,12 +85,40 @@ class GameOverView: UIView {
         label.layer.borderWidth = borderWidth
         label.textAlignment = .center
         label.layer.borderColor = UIColor.white.cgColor
-        
         return label
+    }
+    
+    //Create Buttons:
+    func createButton(name: String, textSize: CGFloat, borderWidth: CGFloat) -> UIButton {
+        let button = UIButton()
+        button.setTitle(name, for: UIControlState.normal)
+        button.titleLabel?.font = UIFont(name: "Chalkduster", size: textSize)
+        button.layer.cornerRadius = 8
+        button.layer.borderWidth = borderWidth
+        button.titleLabel?.textAlignment = .center
+        button.layer.borderColor = UIColor.white.cgColor
+        button.setTitleColor(UIColor.white, for: UIControlState.normal)
+        button.setTitleColor(UIColor(red: 83/255, green: 83/255, blue: 83/255, alpha: 1), for: UIControlState.highlighted)
+        button.setBackgroundImage(UIImage.fromColor(color: UIColor.white), for: UIControlState.highlighted)
+        return button
     }
     
     //Rematch Label was tapped:
     func didTapRestart() {
         self.isHidden = true
+    }
+}
+
+//Use UIColor as a UIImage Background:
+extension UIImage {
+    static func fromColor(color: UIColor) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()
+        context!.setFillColor(color.cgColor)
+        context!.fill(rect)
+        let img = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return img!
     }
 }
